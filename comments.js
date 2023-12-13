27bac1266be8101ec7cc8d3d9e070a1349bd7cd0
Dulche <115841388+kevin-dulche@users.noreply.github.com>
2023-12-13 00:14:53 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,24 @@
+// Create a web server
+// Create an API endpoint that accepts a POST request to /api/comments
+// Create a web form that submits a POST request to /api/comments
+// Create a web form that submits a DELETE request to /api/comments/:id
+// Create a web form that submits a PUT request to /api/comments/:id
+
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var fs = require('fs');
+
+// Set up express app
+var app = express();
+var PORT = 3000;
+
+// Set up express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Data
+var comments = [
+  {
+    id: 1,
